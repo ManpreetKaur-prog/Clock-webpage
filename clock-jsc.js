eef75b34c3641a36a0ff224c3befc41aee38f412
ManpreetKaur-prog <72974411+ManpreetKaur-prog@users.noreply.github.com>
2021-10-24 23:26:23 +0530

    Add files via upload

---
+++ clock-jsc.js
@@ -0,0 +1,35 @@
+var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+setInterval(showTime, 1000);
+        function showTime() {
+            let time = new Date();
+            let hour = time.getHours();
+            let min = time.getMinutes();
+            let sec = time.getSeconds();
+            am_pm = "AM";
+  
+            if (hour > 12) {
+                hour -= 12;
+                am_pm = "PM";
+            }
+            if (hour == 0) {
+                hr = 12;
+                am_pm = "AM";
+            }
+  
+            hour = hour < 10 ? "0" + hour : hour;
+            min = min < 10 ? "0" + min : min;
+            sec = sec < 10 ? "0" + sec : sec;
+  
+            let currentTime = hour + ":" 
+                + min + ":" + sec + am_pm;
+  
+            document.getElementById("clock")
+                .innerHTML = currentTime;
+            let currentDay = daylist[time.getDay()];
+            let currentMonth = monthNames[time.getMonth()];
+            let currentYear = time.getFullYear()
+            document.getElementById("day").innerHTML = currentDay+", "+currentMonth+" "+currentYear;
+        }
+  
+        showTime();
